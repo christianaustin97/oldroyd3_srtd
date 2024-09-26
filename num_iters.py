7000d66301c565c6dc66cc56412af3f71d14f71b
christianaustin97 <christian.austin97@gmail.com>
2024-09-26 16:38:53 -0400

    test comment

--- num_iters.py
+++ num_iters.py
@@ -2,3 +2,5 @@
 Generates some plots and tables for number of SRTD iters it takes to get to 
 different tolerance values/residual values/difference between consecutive iterates
 """
+
+# Test comment ignore me
