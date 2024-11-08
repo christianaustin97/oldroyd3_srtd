6ad50588073098ed33e0d80c5b5a346f68f6febe
christianaustin97 <christian.austin97@gmail.com>
2024-11-08 00:37:58 -0500

    Few Weissenberg number searches for Corotational model

--- binary_search_Wi.py
+++ binary_search_Wi.py
@@ -16,8 +16,6 @@ h_ldc = h_jb/2.0
 eta=1.0
 a=1.0 # 1 for UCM, 0 for corotational
 
-speed=1.0
-
 max_srtd_iters = 20
 srtd_tol = 1e-8
 
@@ -27,41 +25,65 @@ Wi_tol = 1e-4 # how close to we want our approx of max Wi to be
 rad = 0.5
 ecc = 0.25
 
+speed=1.0
 
-# SRTD, JB problem
-l1_low_jb = 0.1
-l1_high_jb = 2.0
+# EVSS l1 limits
+evss_jb_l1 = [0.05, 0.2]
+evss_ldc_l1 = [0.05, 0.2]
 
-l1_low_ldc = 0.05
-l1_high_ldc = 1.5
-n = np.ceil(np.log2((l1_high_jb-l1_low_jb)/Wi_tol)).astype(int)
+# SRTD l1 limits
+srtd_jb_l1 = [0.01, 0.1]
+srtd_ldc_l1 = [0.01, 0.1]
+
+n1 = np.ceil(np.log2((evss_jb_l1[1]-evss_jb_l1[0])/Wi_tol)).astype(int)
+n2 = np.ceil(np.log2((srtd_jb_l1[1]-srtd_jb_l1[0])/Wi_tol)).astype(int)
+n=max([n1, n2])
 
 for i in range(n):
     # Binary search 
-    l1_jb = (l1_high_jb+l1_low_jb)/2.0
-    mu1_jb = a*l1_jb
-    print("current l1 for jb: %.5f"%l1_jb)
+    current_evss_jb_l1 = (evss_jb_l1[0] + evss_jb_l1[1])/2.0
+    current_evss_ldc_l1 = (evss_ldc_l1[0] + evss_ldc_l1[1])/2.0
+    current_srtd_jb_l1 = (srtd_jb_l1[0] + srtd_jb_l1[1])/2.0
+    current_srtd_ldc_l1 = (srtd_ldc_l1[0] + srtd_ldc_l1[1])/2.0
 
-    l1_ldc = (l1_high_ldc+l1_low_ldc)/2.0
-    mu1_ldc = a*l1_ldc
-    print("current l1 for ldc: %.5f"%l1_ldc)
+    mu1 = 0.0 # Corotatioal model
 
-    soln_jb = oldroyd_3_EVSS.oldroyd_3_JB_EVSS(h_jb, rad, ecc, speed, eta, l1_jb, mu1_jb)
-    soln_ldc = oldroyd_3_EVSS.oldroyd_3_LDC_EVSS(h_ldc, speed, eta, l1_ldc, mu1_ldc)
+    print("current l1 for evss jb: %.5f"%current_evss_jb_l1)
+    print("current l1 for evss ldc: %.5f"%current_evss_ldc_l1)
+    print("current l1 for srtd jb: %.5f"%current_srtd_jb_l1)
+    print("current l1 for srtd ldc: %.5f"%current_srtd_ldc_l1)
 
-    #soln_jb = oldroyd_3_SRTD.oldroyd_3_JB_SRTD(h_jb, rad, ecc, speed, eta, l1_jb, mu1_jb, max_srtd_iters, srtd_tol)
-    #soln_ldc = oldroyd_3_SRTD.oldroyd_3_LDC_SRTD(h_ldc, speed, eta, l1_ldc, mu1_ldc, max_srtd_iters, srtd_tol)
-    if(soln_jb.converged):
-        l1_low_jb = l1_jb
+
+    evss_jb = oldroyd_3_EVSS.oldroyd_3_JB_EVSS(h_jb, rad, ecc, speed, eta, current_evss_jb_l1, mu1)
+    evss_ldc = oldroyd_3_EVSS.oldroyd_3_LDC_EVSS(h_ldc, speed, eta, current_evss_ldc_l1, mu1)
+
+    srtd_jb = oldroyd_3_SRTD.oldroyd_3_JB_SRTD(h_jb, rad, ecc, speed, eta, current_srtd_jb_l1, mu1, max_srtd_iters, srtd_tol)
+    srtd_ldc = oldroyd_3_SRTD.oldroyd_3_LDC_SRTD(h_ldc, speed, eta, current_srtd_ldc_l1, mu1, max_srtd_iters, srtd_tol)
+
+    if(evss_jb.converged):
+        evss_jb_l1[0] = current_evss_jb_l1
     else:
-        l1_high_jb = l1_jb
+        evss_jb_l1[1] = current_evss_jb_l1
 
-    if(soln_ldc.converged):
-        l1_low_ldc = l1_ldc
+    if(evss_ldc.converged):
+        evss_ldc_l1[0] = current_evss_ldc_l1
     else:
-        l1_high_ldc = l1_ldc
+        evss_ldc_l1[1] = current_evss_ldc_l1
+
+    if(srtd_jb.converged):
+        srtd_jb_l1[0] = current_srtd_jb_l1
+    else:
+        srtd_jb_l1[1] = current_srtd_jb_l1
+
+    if(srtd_ldc.converged):
+        srtd_ldc_l1[0] = current_srtd_ldc_l1
+    else:
+        srtd_ldc_l1[1] = current_srtd_ldc_l1
 
     
-    print("Max l1 for EVSS, JB, speed=%.1f, is between %.5f and %.5f"%(speed, l1_low_jb, l1_high_jb))
-    print("Max l1 for EVSS, LDC, speed=%.1f, is between %.5f and %.5f"%(speed, l1_low_ldc, l1_high_ldc))
+    print("Max l1 for EVSS, JB, speed=%.1f, is between %.5f and %.5f"%(speed, evss_jb_l1[0], evss_jb_l1[1]))
+    print("Max l1 for EVSS, LDC, speed=%.1f, is between %.5f and %.5f"%(speed, evss_ldc_l1[0], evss_ldc_l1[1]))
+    print("Max l1 for SRTD, JB, speed=%.1f, is between %.5f and %.5f"%(speed, srtd_jb_l1[0], srtd_jb_l1[1]))
+    print("Max l1 for SRTD, LDC, speed=%.1f, is between %.5f and %.5f"%(speed, srtd_ldc_l1[0], srtd_ldc_l1[1]))
+
 # 
